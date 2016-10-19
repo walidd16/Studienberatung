842fb1a079841879297f7fe99b3b96d6df4639ca
Abdul <Abdul>
2016-10-19 05:53:49 +0200

    frage und scanner

---
+++ Studienberatung.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Studienberatung {
+	
+
+public static void main(String[] args) {
+	System.out.print("Hast du Abitur?");
+	Scanner scan = new Scanner(System.in);
+	scan.next();
+	}
+}
