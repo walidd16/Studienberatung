3834482e81e57079523f54cf21b853fd39e4af7d
Abdul <Abdul>
2016-10-19 06:48:26 +0200

    fachabitur frage

--- Studienberatung.java
+++ Studienberatung.java
@@ -7,5 +7,13 @@ public static void main(String[] args) {
 	System.out.print("Hast du Abitur?");
 	Scanner scan = new Scanner(System.in);
 	String eingabe = scan.next();
+	
+		if (eingabe.equalsIgnoreCase("n")){
+			System.out.println("Hast du Fachabitur?");
+			String eingabe = scan.next();
+		}
+	
+
+	
 	}
 }
