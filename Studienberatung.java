10ed134cb80c7dee651a0e3bd51633f4d33de013
Abdul <Abdul>
2016-10-19 06:09:20 +0200

    eingabestring added

--- Studienberatung.java
+++ Studienberatung.java
@@ -6,6 +6,6 @@ public class Studienberatung {
 public static void main(String[] args) {
 	System.out.print("Hast du Abitur?");
 	Scanner scan = new Scanner(System.in);
-	scan.next();
+	String eingabe = scan.next();
 	}
 }
