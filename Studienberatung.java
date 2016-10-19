37e6230458d67bf3ea78290b86d2c794fa2e22c2
Abdul <Abdul>
2016-10-19 06:59:30 +0200

    bist du wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -7,5 +7,11 @@ public static void main(String[] args) {
 	System.out.print("Hast du Abitur?");
 	Scanner scan = new Scanner(System.in);
 	String eingabe = scan.next();
+		if (eingabe.equalsIgnoreCase("j")){
+			System.out.println("Bist du wissbegierig?");
+			String eingabe2 = scan.next();
+
+		}
+		
 	}
 }
