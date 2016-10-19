6090850bfe561cb2c307fae281b8346b4826ded8
Abdul <Abdul>
2016-10-19 07:23:48 +0200

    berufsausbildung

--- Studienberatung.java
+++ Studienberatung.java
@@ -11,6 +11,11 @@ public static void main(String[] args) {
 		if (eingabe.equalsIgnoreCase("n")){
 			System.out.println("Hast du Fachabitur?");
 			String eingabe2 = scan.next();
+			
+			if (eingabe2.equalsIgnoreCase("j")){
+				System.out.println("Hast du eine Berufsausbildung?");
+				String eingabe3 = scan.next();
+			}
 		}
 
 		else if (eingabe.equalsIgnoreCase("j")){
